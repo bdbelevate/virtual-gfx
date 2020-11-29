f7049e1cf882c54d0f67d39346444f1d5970f181
Brian DeBoer <bdeboer@experiencecosm.com>
2020-11-29 08:37:21 -0800

    remove some stuff

--- Assets/Scripts/SocketScript.cs
+++ Assets/Scripts/SocketScript.cs
@@ -1,32 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using SocketIOClient;
+// using SocketIOClient;
 
 public class SocketScript : MonoBehaviour
 {
     public Jumbotron jumbotron;
 
     // Start is called before the first frame update
-    async void Start()
+    void Start()
     {
         Debug.Log("Connection made to socketIO");
-        var client = new SocketIO("http://127.0.0.1:4001");
-        client.On("inLeft", response => {
-            jumbotron.InLeft();
-        });
-        client.On("outLeft", response => {
-            jumbotron.OutLeft();
-        });
-        client.On("inRight", response => {
-            jumbotron.InRight();
-        });
-        client.On("outRight", response => {
-            jumbotron.OutRight();
-        });
-        client.OnConnected += async (sender, e) => {
-            await client.EmitAsync("unity");
-        };
-        await client.ConnectAsync();
+        // var client = new SocketIO("http://127.0.0.1:4001");
+        // client.On("inLeft", response => {
+        //     jumbotron.InLeft();
+        // });
+        // client.On("outLeft", response => {
+        //     jumbotron.OutLeft();
+        // });
+        // client.On("inRight", response => {
+        //     jumbotron.InRight();
+        // });
+        // client.On("outRight", response => {
+        //     jumbotron.OutRight();
+        // });
+        // client.OnConnected += async (sender, e) => {
+        //     await client.EmitAsync("unity");
+        // };
+        // await client.ConnectAsync();
     }
 }
